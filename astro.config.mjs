7449c2318386abd6a0e0a84cae85f3036f4586e7
Benjamin Shuster <bentzishuster613@gmail.com>
2025-03-17 14:51:11 -0400

    fix: update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   base: '/personal-site/',
+  site: "https://bentzi-shuster.github.io/personal-site/",
+  
   vite: {
     plugins: [tailwindcss()]
   }
